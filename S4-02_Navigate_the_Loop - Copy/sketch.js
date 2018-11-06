2cca1cc12c04d96dbdcb1422c9f7908997122cd4
victorchan764 <victorchan764@gmail.com>
2018-11-06 16:32:25 +0000

    Updates

---
+++ S4-02_Navigate_the_Loop - Copy/sketch.js
@@ -0,0 +1,46 @@
+//DAT405 / GAD405
+//04_Exercise01 - Loops and Random Color
+
+let size = 100;
+let backImg;
+let xPlusOne = 0;
+let yPlusOne = 0;
+
+function setup() {
+  createCanvas(500, 500);
+  frameRate(30); //Drawing speed 3 fps
+  backImg = loadImage("patternXY.png");
+  backImg.resize(500,500);
+}
+
+function draw() {
+  //background(80);
+  ellipses(xPlusOne, yPlusOne);
+
+  //Run the nested loop for x, and y
+  //for (let y = 0; y < 5; y++) {
+    //for (let x = 0; x < 5; x++) {
+      //rects (Math.round(random(4)), Math.round(random(4)));
+    //}
+  //}
+}
+
+function ellipses(x, y){
+  var x = random(500);
+  var y = random(500);
+  var size1 = random(200);
+  //Background rect()
+  fill(random(255), random(255), random(255), random(255));
+  ellipse(x, y, size1, size1);
+
+  //Draw the foreground shapes with a random RGB color
+  //Create a slight offeset for the x and y, to appear in the middle
+;}
+
+function keyPressed() {
+  if (key == 'd' || key == 'D') xPlusOne++;
+  if (key == 'a' || key == 'A') xPlusOne--;
+  if (key == 's' || key == 'S') yPlusOne++;
+  if (key == 'w' || key == 'W') yPlusOne--;
+  console.log("x: " +xPlusOne+ ", y: " +yPlusOne);
+}
