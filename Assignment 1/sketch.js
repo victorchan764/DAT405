552f8cae691f9198be4829d15c4d6c9ac39b4b46
victorchan764 <victorchan764@gmail.com>
2018-11-19 11:14:30 +0000

    Updates

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -1,21 +1,21 @@
 //Define Variables
-//X value is the number of tiles on the X axis
-//Y value is the number of tiles on the X axis
-var tileCountX = 25;
-var tileCountY = 10;
+//tileCountX is the number of tiles on the X axis
+//tileCountY is the number of tiles on the X axis
+let tileCountX = 25;
+let tileCountY = 10;
 
-var hueValues = [];
-var saturationValues = [];
-var brightnessValues = [];
+let hueValues = [];
+let saturationValues = [];
+let brightnessValues = [];
 
 //Set canvas size and Color mode
 function setup() {
   createCanvas(594, 841);
   colorMode(HSB, 360, 100, 100, 100);
-  noStroke();
+  strokeWeight(5);
 
   // init with random values
-  for (var i = 0; i < tileCountX; i++) {
+  for (let i = 0; i < tileCountX; i++) {
     hueValues[i] = random(360);
     saturationValues[i] = random(100);
     brightnessValues[i] = random(100);
@@ -24,26 +24,27 @@ function setup() {
 
 function draw() {
   // Set the background to Black
-  background(0);
+  background(0, 0, 0);
 
   // The mouse coordinates is limited to the canvas size
-  var mX = constrain(mouseX, 0, width);
-  var mY = constrain(mouseY, 0, height);
+  let mX = constrain(mouseX, 0, width);
+  let mY = constrain(mouseY, 0, height);
 
   // tile counter
-  var counter = 0;
+  let counter = 0;
 
-  // Mouse is mapped to grid resolution
-  var currentTileCountX = int(map(mX, 0, width, 1, tileCountX));
-  var currentTileCountY = int(map(mY, 0, height, 1, tileCountY));
-  var tileWidth = width / currentTileCountX;
-  var tileHeight = height / currentTileCountY;
+  // The movement of the Mouse is mapped to grid resolution
+  let currentTileCountX = int(map(mX, 0, width, 1, tileCountX));
+  let currentTileCountY = int(map(mY, 0, height, 1, tileCountY));
+  let tileWidth = width / currentTileCountX;
+  let tileHeight = height / currentTileCountY;
 
-  for (var gridY = 0; gridY < tileCountY; gridY++) {
-    for (var gridX = 0; gridX < tileCountX; gridX++) {
-      var posX = tileWidth * gridX;
-      var posY = tileHeight * gridY;
-      var index = counter % currentTileCountX;
+  for (let gridY = 0; gridY < tileCountY; gridY++) {
+    for (let gridX = 0; gridX < tileCountX; gridX++) {
+      //Wherever the mouse is positioned, the tile sizes increase or decrease
+      let posX = tileWidth * gridX;
+      let posY = tileHeight * gridY;
+      let index = counter % currentTileCountX;
 
       // Get the component color values in the index
       fill(hueValues[index], saturationValues[index], brightnessValues[index]);
@@ -55,13 +56,13 @@ function draw() {
 //If key is pressed different colours will appear
 function keyPressed() {
 
-    var colors = [];
-    for (var i = 0; i < hueValues.length; i++) {
+    let colors = [];
+    for (let i = 0; i < hueValues.length; i++) {
       colors.push(color(hueValues[i], saturationValues[i], brightnessValues[i]));
     }
 
   if (key == '1') {
-    for (var i = 0; i < tileCountX; i++) {
+    for (let i = 0; i < tileCountX; i++) {
       hueValues[i] = random(100);
       saturationValues[i] = random(100);
       brightnessValues[i] = random(100);
@@ -69,7 +70,7 @@ function keyPressed() {
   }
 
   if (key == '2') {
-    for (var i = 0; i < tileCountX; i++) {
+    for (let i = 0; i < tileCountX; i++) {
       hueValues[i] = random(150);
       saturationValues[i] = random(110);
       brightnessValues[i] = 110;
@@ -77,34 +78,34 @@ function keyPressed() {
   }
 
   if (key == '3') {
-    for (var i = 0; i < tileCountX; i++) {
+    for (let i = 0; i < tileCountX; i++) {
       hueValues[i] = random(200);
-      saturationValues[i] = 120;
+      saturationValues[i] = random(120);
       brightnessValues[i] = random(120);
     }
   }
 
   if (key == '4') {
-    for (var i = 0; i < tileCountX; i++) {
-      hueValues[i] = 250;
-      saturationValues[i] = 130;
+    for (let i = 0; i < tileCountX; i++) {
+      hueValues[i] = random(250);
+      saturationValues[i] = random(130);
       brightnessValues[i] = random(130);
     }
   }
 
   if (key == '5') {
-    for (var i = 0; i < tileCountX; i++) {
-      hueValues[i] = 300;
-      saturationValues[i] = 140;
+    for (let i = 0; i < tileCountX; i++) {
+      hueValues[i] = random(300);
+      saturationValues[i] = random(140);
       brightnessValues[i] = random(140);
     }
   }
 
   if (key == '6') {
-    for (var i = 0; i < tileCountX; i++) {
-      hueValues[i] = 360;
+    for (let i = 0; i < tileCountX; i++) {
+      hueValues[i] = random(360);
       saturationValues[i] = random(150);
-      brightnessValues[i] = 150;
+      brightnessValues[i] = random(150);
     }
   }
 }
