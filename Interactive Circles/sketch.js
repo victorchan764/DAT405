e05a20f9655d82b1c90fd2114c6c9c16ba1f479c
victorchan764 <victorchan764@gmail.com>
2018-11-27 14:09:35 +0000

    Updates

--- Interactive Circles/sketch.js
+++ Interactive Circles/sketch.js
@@ -5,6 +5,7 @@ function setup() {
 }
 
 function draw() {
+//The circle colour is always changing to a random colour in the RGB scale
 stroke(random(255),random(255),random(255));
 fill(random(255),random(255),random(255));
 //Creates a trail of circles based on the movement of the mouse
