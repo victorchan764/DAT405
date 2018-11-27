915137b71ae763b0f29dc11d3f6e55c2585e8e5e
victorchan764 <victorchan764@gmail.com>
2018-11-27 14:05:02 +0000

    Updates

--- Interactive Circles/sketch.js
+++ Interactive Circles/sketch.js
@@ -1,4 +1,4 @@
-
+//setting up canvas
 function setup() {
   createCanvas(500,500);
   background (0);
@@ -7,5 +7,6 @@ function setup() {
 function draw() {
 stroke(random(255),random(255),random(255));
 fill(random(255),random(255),random(255));
+//Creates a trail of circles based on the movement of the mouse
 ellipse(mouseX,mouseY,50,50);
 }
