61b19ba6a268dd666c2fb427b18a47bad16bda41
victorchan764 <victorchan764@gmail.com>
2018-12-03 10:34:57 +0000

    Updates

--- 01_TemperatureBasic - Copy/01_TemperatureBasic/sketch.js
+++ 01_TemperatureBasic - Copy/01_TemperatureBasic/sketch.js
@@ -2,6 +2,9 @@
 
 //Use a text/string object to assign the city we would like to know the weather of
 let getLocation = "Plymouth";
+let getLocationL = "London";
+let getLocationA = "Athens";
+let getLocationS = "Shanghai";
 //The API key has to be given by the openweathermap.org website (for free / per account)
 let apiKey = "6b4a465ac9894c63172283b3f271c20c";
 //This variable will store the JSON weather data
@@ -14,25 +17,36 @@ function preload() {
   //http://api.openweathermap.org
   //The text/string object is formatted with the location we want to use, and our own API key
   let url = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation+"&units=metric&appid="+apiKey;
+  let url1 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation+"&units=metric&appid="+apiKey;
+  let url2 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation+"&units=metric&appid="+apiKey;
+  let url3 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation+"&units=metric&appid="+apiKey;
+
   //The URL is sent to the loadJSON that returns the data to the weather variable
   weather = loadJSON(url);
+  weather1 = loadJSON(url);
+  weather2 = loadJSON(url);
+  weather2 = loadJSON(url);
 }
 
 function setup() {
-  console.log(weather); //Return all JSON data
-  console.log("Location: " + getLocation) //Show the location we are searching
-  console.log("Temperature: " + weather.main.temp + "°C"); //Show location's conditions (temperature)
-  console.log("Temperature (min): " + weather.main.temp_min + "°C"); //Minimum temperature
-  console.log("Temperature (max): " + weather.main.temp_max + "°C"); //Maximum temperature
-  console.log("Humidity: " + weather.main.humidity); //Humidity
-  console.log("Pressure: " + weather.main.pressure); //Pressure
+  console.log(weather); //Return all JSON data'
+  console.log()
+  //console.log("Location: " + getLocation) //Show the location we are searching
+  //console.log("Temperature: " + weather.main.temp + "°C"); //Show location's conditions (temperature)
+  //console.log("Temperature (min): " + weather.main.temp_min + "°C"); //Minimum temperature
+  //console.log("Temperature (max): " + weather.main.temp_max + "°C"); //Maximum temperature
+  //console.log("Humidity: " + weather.main.humidity); //Humidity
+  //console.log("Pressure: " + weather.main.pressure); //Pressure
 
   //Display temperature information on the screen
   createCanvas(400, 400);
   background(160);
   textAlign(CENTER);
   textSize(18);
-  text(getLocation + weather.main.temp + "°C", width/2, height/2);
+  text("Currrent temperature in Plymouth" + weather.main.temp + "°C", width/2, height/2);
+  text("Currrent temperature in London" + weather1.main.temp + "°C", width/2, height/3);
+  text("Currrent Humidity in Athens" + weather2.main.humidity + "%", width/2, height/4);
+  text("Currrent Wind speed in Shanghai" + weather.wind.speed + "MPH", width/2, height/5);
   noLoop();
 }
 
