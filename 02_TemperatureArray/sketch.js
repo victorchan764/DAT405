a65fe85ac5395f432b13a82a147df3a19d9fe5b8
victorchan764 <victorchan764@gmail.com>
2018-12-04 16:28:07 +0000

    Updates

--- 02_TemperatureArray/sketch.js
+++ 02_TemperatureArray/sketch.js
@@ -13,8 +13,8 @@ function preload() {
   //This random number will be between 0 and the location.Array.lengh-1 (that is from 0 to 5)
   //Based on the random number selected, the city that is saved in that array position will return to randomLocation
   //randomLocation = locationArray[round(random(locationArray.length-1))];
-  for (let i = 0; i < LocationArray.length; i++) {
-    let url = "http://api.openweathermap.org/data/2.5/weather?q="+randomLocation+"&units=metric&appid="+apiKey;
+  for (let i = 0; i < locationArray.length; i++) {
+    let url = "http://api.openweathermap.org/data/2.5/weather?q="+locationArray[i]+"&units=metric&appid="+apiKey;
     weather[i] = loadJSON(url);
     console.log(weather[i]);
   }
@@ -29,28 +29,31 @@ function setup() {
   //console.log("Pressure: " + weather.main.pressure);
 
   createCanvas(600, 600);
-  textAlign(CENTER);
+  //textAlign(CENTER);
   textSize(24);
   noLoop();
 }
 
 function draw() {
-  let mapColor [];
+  let mapColor = [];
   //According to the data returned from the JSON object, we use the current temperature of the city
   //to control the background color. The following conditional statements will evaluate only one option (thus, one color)
   for (let i=0; i<weather.length; i++){
 
   if (weather[i].main.temp<-5){
     mapColor[i] = color(36, 194, 203, 255);
-  } else if (weather[i].main.temp<-5 && weather[i].main.temp<5){
-    mapColor [i] = color(196, 208, 234, 255);
-  } else if (weather.main.temp>5 && weather.main.temp<10){
-    mapColor = color(234, 231, 222, 255);
-  } else if (weather[i].main.temp<10 && weather[i].main.temp<20){
-    mapColor [i] = color(234, 208, 155, 255);
   }
-  else if ( weather[i].main.temp>20){
-  mapColor [i] = color(234, 143, 0, 255);
+  if (weather[i].main.temp>-5 && weather[i].main.temp<=5){
+    mapColor[i] = color(196, 208, 234, 255);
+  }
+  if (weather[i].main.temp>5 && weather[i].main.temp<10){
+    mapColor[i] = color(234, 231, 222, 255);
+  }
+  if (weather[i].main.temp>10 && weather[i].main.temp<20){
+    mapColor[i] = color(234, 208, 155, 255);
+  }
+  if ( weather[i].main.temp>20){
+  mapColor[i] = color(234, 143, 0, 255);
   }
 
   var pos = i * 200
@@ -61,20 +64,21 @@ function draw() {
     fill(0);
     text(locationArray[i], pos+10, height/6-30);
     text("Temperature: " + weather[i].main.temp + "°C", pos+10, height/6);
-    text("Humidity ": " + weather[i].main.humidity + "°%", pos+10, height/6+30);
+    text("Humidity: "  + weather[i].main.humidity + "°%", pos+10, height/6+30);
   }
   else {
+    console.log(mapColor[i]);
     fill(mapColor[i]);
     rect(pos-600, 300, width/2, height/2);
     fill(0);
     text(locationArray[i], pos-590, height-180);
     text("Temperature: " + weather[i].main.temp + "°C", pos-590, height-150);
-    text("Humidity ": " + weather[i].main.humidity + "°%", pos-590, height-120)
+    text("Humidity: "  + weather[i].main.humidity + "°%", pos-590, height-120)
   }
   //background(mapColor);
   //fill(0);
   //text(randomLocation, width/2, height/2-30);
   //text("Temperature: " + weather.main.temp + "°C", width/2, height/2-0);
   //text("Humidity: " + weather.main.humidity + "%", width/2, height/2+30);
-}
+  }
 }
