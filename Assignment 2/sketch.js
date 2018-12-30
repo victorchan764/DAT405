ea92a673bb8615772202e76f372067dfd086ce30
victorchan764 <victorchan764@gmail.com>
2018-12-30 15:52:42 +0000

    Updates

--- Assignment 2/sketch.js
+++ Assignment 2/sketch.js
@@ -1,150 +1,40 @@
-let apikey =  "f16e6488b692bc7ce5f8b1940c60edda"
-let weather,
-  cityName,
-  country,
-  weatherId,
-  weatherDescription,
-  cloudiness,
-  humidity,
-  windSpeed,
-  windDeg,
-  temp,
-  visibility,
-  windRatio;
+//http://api.openweathermap.org
+"http://api.openweathermap.org/data/2.5/weather?q="+"Yeovil"+"&units=metric&appid="+"6b4a465ac9894c63172283b3f271c20c"
+//Use a text/string object to assign the city we would like to know the weather of
+let getLocation = "Yeovil";
+//The API key has to be given by the openweathermap.org website (for free / per account)
+let apiKey = "6b4a465ac9894c63172283b3f271c20c";
+//This variable will store the JSON weather data
+let weather;
 
-let city = "Yeovil";
-let r = 0;
-
-let epochUpdate, update, updateText;
-let xDir, yDir, unit, countX, countY, size;
-let c;
-
-
+//The preload function is executed before initializing the code in setup
+//Loads any related data or media files
 function preload() {
-  let url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid= "+apikey;
+  //The URL is formatted according to the documentation provided by the developers in:
+  //http://api.openweathermap.org
+  //The text/string object is formatted with the location we want to use, and our own API key
+  let url = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation+"&units=metric&appid="+apiKey;
+  //The URL is sent to the loadJSON that returns the data to the weather variable
   weather = loadJSON(url);
 }
 
 function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.parent("homeAnim");
-  background(0);
-  frameRate(30);
+  console.log(weather); //Return all JSON data
+  console.log("Location: " + getLocation) //Show the location we are searching
+  console.log("Temperature: " + weather.main.temp + "°C"); //Show location's conditions (temperature)
+  console.log("Temperature (min): " + weather.main.temp_min + "°C"); //Minimum temperature
+  console.log("Temperature (max): " + weather.main.temp_max + "°C"); //Maximum temperature
+  console.log("Humidity: " + weather.main.humidity); //Humidity
+  console.log("Pressure: " + weather.main.pressure); //Pressure
 
-  weatherVar();
-
-  /* Orientation vent */
-  angleMode(DEGREES);
-  if (windDeg === undefined || !windDeg) windDeg = 0;
-  xDir = sin(windDeg);
-  yDir = -cos(windDeg);
-
-  unit = round(map(cloudiness, 0, 100, 80, 20));
-  countX = round(windowWidth / unit);
-  countY = round(windowHeight / unit);
-  size = round(windowWidth / countX);
-
-  /*Humidity*/
-  alpha = visibility;
-  c = color(0, 0, 0, alpha);
-
-  /* Convert Epoch to date */
-  update = new Date(epochUpdate * 1000);
-  updateText = update.getHours() + ":" + update.getMinutes();
-
-}
-
-function draw() {
-  fill(c);
-  rect(width/2, height/2, width, height);
-
-  r = r + windRatio;
-  if (r > 2) r = 0;
-  speed = round(r * PI * 100) / 100;
-
-  angleMode(RADIANS);
-  rectMode(CENTER);
-  ellipseMode(CENTER);
-  for (let x = 0; x < countX + 1; x++) {
-    for (let y = 0; y < countY + 1; y++) {
-      push();
-      fixed(x,y);
-      animate(x, y);
-      sketch(x,y);
-      pop();
-    }
-  }
-
-  push();
-  fill(255);
-  textFont("Lato");
-  textAlign(LEFT);
-  textSize(48);
-  text(temp + "°", 20, 60);
-  textSize(14);
-  fill(127);
-  //text(deviceOrientation, 20, 30);
-  text(cityName + " / " + country + " / "+temp + "°", 20, height - 30);
+  //Display temperature information on the screen
+  createCanvas(400, 400);
+  background(160);
   textAlign(CENTER);
-  text(
-    weatherDescription + " / " + weatherId + " / " + windSpeed + "m/s",
-    width / 2,
-    height - 30
-  );
-  textAlign(RIGHT);
-  text(updateText, width - 20, height - 30);
-  pop();
-
-  let ep = 5;
-  frame(0,0,width,ep);
-  frame(0,0,ep,height);
-  frame(width-ep, 0, ep, height);
-  frame(0, height-ep, width, ep);
+  textSize(18);
+  text("Current Temperature in" + getLocation " is " + weather.main.temp + "°C", width/2, height/2);
+  noLoop();
 }
 
-function fixed(x,y) {
-  strokeWeight(1);
-  stroke(255,255,255,30);
-  noFill();
-  //rect(x*size,y*size,size, size);
-  //ellipse(x*size,y*size,size, size);
-}
-
-function sketch(x,y) {
-  strokeWeight(3);
-  stroke(255);
-  point(0, size/2);
-}
-function animate(x, y) {
-  translate(x * size, y * size);
-  rotate(speed + x * xDir + y * yDir);
-}
-
-function frame(posx, posy, widthSize, heightSize){
-  push();
-  fill(0);
-  noStroke();
-  rect(posx,posy,widthSize,heightSize);
-  pop();
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  countX = round(windowWidth / unit);
-  countY = round(windowHeight / unit);
-}
-
-function weatherVar(){
-  cityName = weather.name;
-  country = weather.sys.country;
-  weatherId = weather.weather[0].id;
-  weatherDescription = weather.weather[0].description;
-  temp = round(weather.main.temp);
-  epochUpdate = weather.dt;
-  cloudiness = weather.clouds.all;
-  windSpeed = weather.wind.speed;
-  windRatio = windSpeed / 200;
-  windDeg = weather.wind.deg;
-  visibility = map(weather.visibility, 0, 10000, 0, 255);
-  humidity = weather.main.humidity;
+function draw(){
 }
