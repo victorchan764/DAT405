8ba69a368386f0280d33eab7127ce126ec0bec6d
victorchan764 <victorchan764@gmail.com>
2019-02-15 11:11:32 +0000

    Updates

--- Insta Feed/js/script.js
+++ Insta Feed/js/script.js
@@ -1,6 +1,6 @@
 var feed = new Instafeed({
   get: 'user',
-  userId: '1475372546  ',
+  userId: '1475372546',
   accessToken: '1475372546.d3402f4.827385c9f5cb4c558de49b5d623ceb5a',
   filter: function(image) {
   var MAX_LENGTH = 40;
@@ -16,6 +16,6 @@ var feed = new Instafeed({
   // ensure the filter doesn't reject any images
   return true;
 },
-  template: '<div class="col-2"><img class="w-100" src={{image}}><hr><p class="likes"><i class="fas fa-heart"></i>{{likes}} <br> <i class="fas fa-magic"></i>{{model.filter}}<br><p>{{model.short_caption}}...<a href="{{link}}" target="_blank">Read More</a></p></div>'
+  template: '<div class="col-2"><img class="w-100" src={{image}}><hr><p class="likes"><i class="far fa-heart"></i>{{likes}} <br> <i class="fas fa-magic"></i>{{model.filter}}<br><p>{{model.short_caption}}...<a href="{{link}}" target="_blank">Read More</a></p></div>'
 });
 feed.run();
