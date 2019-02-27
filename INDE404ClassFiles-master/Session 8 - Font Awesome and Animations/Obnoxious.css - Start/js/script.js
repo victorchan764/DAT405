ca47801f797effc27de538d796e121de69636453
victorchan764 <victorchan764@gmail.com>
2019-02-27 14:04:16 +0000

    Updates

--- INDE404ClassFiles-master/Session 8 - Font Awesome and Animations/Obnoxious.css - Start/js/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  currentYear();
-};
-
-function currentYear(){
-  const date = new Date();
-  const autoDate = document.querySelector('#autoDate');
-  autoDate.textContent = date.getFullYear();
-};
